ca97445540e0b607cfb524623e33f69e109bfe2c
AnuLoveCoding <anusrivas07@gmail.com>
2023-05-21 09:19:03 +0000

    "c++ Project"

--- main.cpp
+++ main.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-
-  char current[1000];
-  char largest[1000];
-  int len = 0;
 
   
-  for(int i = 0; i < n; i++){
-    cin.getline(current, 1000, '\n');
-    int currentlength = strlen(current);
-    if(currentlength > len){
+  // int n;
+  // cin >> n;
+
+  // char current[1000];
+  // char largest[1000];
+  // int len = 0;
+
+  
+  // for(int i = 0; i < n; i++){
+  //   cin.getline(current, 1000, '\n');
+  //   int currentlength = strlen(current);
+  //   if(currentlength > len){
       
-       len = currentlength;
-      strcpy(largest, current);
-    }
-  }
+  //      len = currentlength;
+  //     strcpy(largest, current);
+  //   }
+  // }
   
 }
