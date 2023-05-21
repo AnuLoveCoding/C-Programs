0b6700b8b16a63c66d277222a5187e650f71746c
AnuLoveCoding <anusrivas07@gmail.com>
2023-05-21 01:11:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@C-Programs/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  char current[1000];
+  char largest[1000];
+  int len = 0;
+  
+  for(int i = 0; i < n; i++){
+    cin.getline(current, 1000, '\n');
+    int currentlength = strlen(current);
+    if(currentlength > len){
+       len = currentlength;
+      strcpy(largest, current);
+    }
+  }
+}
